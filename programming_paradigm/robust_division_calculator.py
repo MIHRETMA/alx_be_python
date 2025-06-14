09187d4565a7cbaeb142ef4ba5adac516cb28ca8
Mihret Mekbebe Asamenew <mihret.mekbebe@berhan-bank.local>
2025-06-14 12:20:17 +0300

    Handling Division Gracefully

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,8 +3,8 @@ def safe_divide(numerator, denominator):
         num = float(numerator)
         den = float(denominator)
         if den == 0:
-            print("Error: Cannot divide by zero.")
+            return "Error: Cannot divide by zero."
         else:
-            print(f"The result of the division is {num / den}.")
+            return f"The result of the division is {num / den}"
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return "Error: Please enter numeric values only."
