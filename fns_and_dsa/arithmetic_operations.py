0bf3e1a6a057698f6cab62f1c00625580b5b7208
Mihret Mekbebe Asamenew <mihret.mekbebe@berhan-bank.local>
2025-06-14 09:48:35 +0300

    changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,7 +5,10 @@ def perform_operation(num1, num2, operation):
             return (num1 - num2)
     elif operation == 'multiply':
             return (num1 * num2)
-    elif operation == 'divide' and num2 != 0:
-        return (num1 / num2)
+    elif operation == 'divide':
+        if num2 == 0:
+            print("Invalid operation")
+        else:
+            return (num1 / num2)
     else:
         print("Invalid operation")
