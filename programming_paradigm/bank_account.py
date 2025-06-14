eaed508a7642d667278f316ccc3160dfdd5ab062
Mihret Mekbebe Asamenew <mihret.mekbebe@berhan-bank.local>
2025-06-14 11:44:08 +0300

    Bank Account class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,5 +10,4 @@ class BankAccount:
         else:
             return False
     def display_balance(self):
-        balance = int(self.account_balance)
-        print(f"Current Balance: ${balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
