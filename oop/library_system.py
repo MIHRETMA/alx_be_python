b91bd932d3b53166e3d1c7a5f2d621af9bb5a46c
Mihret Mekbebe Asamenew <mihret.mekbebe@berhan-bank.local>
2025-06-25 19:19:23 +0300

    inheritance

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,18 +12,18 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
     def __repr__(self):
-        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}KB"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
     def __repr__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}KB"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
